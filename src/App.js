437a22b8219bc103459de4300038b9931bb37b6b
nikorockz <arun90rr@gmail.com>
2019-04-14 13:49:55 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+
+      <main className="container mt-4">
+        <h1>Movies</h1>
+      </main>
+      </div>  
+      
+    );
+  }
+}
+
+export default App;
