7d54d7678a446b199bd7f5446693d028d99f4474
nikorockz <arun90rr@gmail.com>
2019-04-16 16:36:44 +0530

    Added basic routing logic in navbar

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 
 import NavBar from './components/navBar';
 import MovieMain from './components/movieMain';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import RegisterForm from './components/registerForm';
+import LoginForm from './components/loginForm';
+import NotFound from './components/commons/notFound';
 
 import './App.css';
 
@@ -13,8 +18,14 @@ class App extends Component {
         <NavBar />
         <main className="container mt-4">
           <Switch>
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies" component={MovieMain} />
+            <Route path="/not-found" component={NotFound} />
             <Redirect from="/" to="/movies" />
+            <Redirect to="/not-found" />
           </Switch>
         </main>
       </div>
