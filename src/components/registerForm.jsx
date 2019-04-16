defbcf33e3b603bfc78cff17bde54968cea6cbc4
nikorockz <arun90rr@gmail.com>
2019-04-16 19:58:07 +0530

    Added logic for Register Form

--- src/components/registerForm.jsx
+++ src/components/registerForm.jsx
@@ -1,8 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class RegisterForm extends Component {
+import Joi from 'joi-browser';
+
+import Form from './commons/form';
+
+class RegisterForm extends Form {
+  state = {
+    data: {
+      username: '',
+      password: '',
+      fullName: ''
+    },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .email()
+      .required()
+      .label('Username'),
+    password: Joi.string()
+      .min(5)
+      .required()
+      .label('Password'),
+    fullName: Joi.string()
+      .required()
+      .label('Name')
+  };
+
+  doSubmit = () => {
+    // submit to server
+    console.log('Submitted');
+  };
+
   render() {
-    return <h1>Register</h1>;
+    return (
+      <React.Fragment>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput('username', 'Username', 'email')}
+          {this.renderInput('password', 'Password', 'password')}
+          {this.renderInput('fullName', 'Name')}
+          {this.renderSubmit('Register')}
+        </form>
+      </React.Fragment>
+    );
   }
 }
 
